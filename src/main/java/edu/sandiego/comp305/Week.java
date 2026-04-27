5a13b1dedee19fc5a1bb12c51ea199cbec936f99
Ian Falkenstrom <ifalkenstrom@sandiego.edu>
2026-04-27 16:02:32 -0700

    Add Week class stub

---
+++ src/main/java/edu/sandiego/comp305/Week.java
@@ -0,0 +1,19 @@
+package edu.sandiego.comp305;
+
+import java.util.List;
+
+public class Week {
+
+    private int weekNumber;
+    private List<Match> matches;
+
+    public Week(int weekNumber, List<Match> matches) {
+    }
+
+    public void simulate(ScoringStrategy strategy) {
+    }
+
+    public List<Match> getMatches() {
+        return null;
+    }
+}
