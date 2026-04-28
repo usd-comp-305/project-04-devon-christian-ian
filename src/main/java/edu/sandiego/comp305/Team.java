d47026efe40d71ed16fa626538f723ecbf1d20ef
devongalb <dgalbraith@sandiego.edu>
2026-04-28 12:21:18 -0700

    fix: removed error causing code fro TradeHistory

--- src/main/java/edu/sandiego/comp305/Team.java
+++ src/main/java/edu/sandiego/comp305/Team.java
@@ -1,4 +1,33 @@
 package edu.sandiego.comp305;
 
+import java.util.List;
+
+/**
+ *
+ *
+ */
 public class Team {
+
+    private String name;
+    private List<Politician> roster;
+
+    public Team(String name) {}
+
+    public void addPolitician(Politician p) {}
+
+    public List<Politician> getRoster() {
+        return null;
+    }
+
+    public double calculateWeeklyScore(ScoringStrategy strategy, int week) {
+        return 0.0;
+    }
+
+    public Politician getMVP(ScoringStrategy strategy, int week) {
+        return null;
+    }
+
+    public String getName() {
+        return null;
+    }
 }
--- src/main/java/edu/sandiego/comp305/TradeHistory.java
+++ src/main/java/edu/sandiego/comp305/TradeHistory.java
@@ -9,48 +9,7 @@ import java.time.temporal.IsoFields;
  */
 public class TradeHistory {
 
-    private List<Trade> trades;
 
-    public TradeHistory() {
-        this.trades = new ArrayList<>();
-    }
+    public double getWeeklyProfit(int week, Map<String, Double> prices) {}
 
-    public void addTrade(Trade trade) {
-        trades.add(trade);
-    }
-
-    public List<Trade> getTrades() {
-        return trades;
-    }
-
-    public List<Trade> getWeeklyTrades(int week) {
-        List<Trade> weeklyTrades = new ArrayList<>();
-
-        for (Trade trade : trades) {
-            int tradeWeek = trade.getDate().get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
-
-            if (tradeWeek == week) {
-                weeklyTrades.add(trade);
-            }
-        }
-
-        return weeklyTrades;
-    }
-
-    public double getWeeklyProfit(int week, Map<String, Double> prices) {
-        double totalProfit = 0.0;
-
-        List<Trade> weeklyTrades = getWeeklyTrades(week);
-
-        for (Trade trade : weeklyTrades) {
-            String ticker = trade.getTicker();
-
-            if (prices.containsKey(ticker)) {
-                double laterPrice = prices.get(ticker);
-                totalProfit += trade.calculateEstimatedProfit(laterPrice);
-            }
-        }
-
-        return totalProfit;
-    }
 }
