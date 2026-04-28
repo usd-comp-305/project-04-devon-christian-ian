abc96380ca6b33373e4af0db128954736011cc14
devongalb <dgalbraith@sandiego.edu>
2026-04-28 15:40:13 -0700

    feat: added test for starting a Team

--- src/test/java/edu/sandiego/comp305/SeasonTest.java
+++ src/test/java/edu/sandiego/comp305/SeasonTest.java
@@ -2,27 +2,28 @@ package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SeasonTest {
 
     @Test
-    void generateSchedule() {
+    void getStandingsStartsAllAtZero() {
+        final List<Team> teams = new ArrayList<>();
+
+        final Team firstTeam = new Team("First");
+        final Team secondTeam = new Team("Second");
+
+        teams.add(firstTeam);
+        teams.add(secondTeam);
+
+        final Season season = new Season(teams);
+
+        assertEquals(0, season.getStandings().get(firstTeam));
+        assertEquals(0, season.getStandings().get(secondTeam));
+        assertEquals(2, season.getStandings().size());
     }
 
-    @Test
-    void runSeason() {
-    }
-
-    @Test
-    void updateStandings() {
-    }
-
-    @Test
-    void getStandings() {
-    }
-
-    @Test
-    void getChampion() {
-    }
 }
