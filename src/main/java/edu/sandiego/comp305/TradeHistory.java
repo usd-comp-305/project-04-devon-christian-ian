34accb48b3bb7617ce01308c7bef632afc385925
Christian Schwerer <cschwerer@sandiego.edu>
2026-05-07 18:03:52 -0700

    TradeHistory refactor: added proper implementation for getWeeklyTrades and getTrades. Also finished the implementation of the construction logic, adding each week of trades to their own ArrayList.
    TradeHistoryTest new: used Mockito to test the logic behind the TradeHistory get Methods to ensure they properly separate the trades and handle edge cases.

--- src/main/java/edu/sandiego/comp305/TradeHistory.java
+++ src/main/java/edu/sandiego/comp305/TradeHistory.java
@@ -1,28 +1,56 @@
 package edu.sandiego.comp305;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
- *
+ * Provides analytics for a Politician's trades,
+ * and functions as a wrapper for computations needed in the scoring strategies.
+ * This helps separate the Trade and Politician classes.
  */
 public class TradeHistory {
 
+    private final Map<Integer, List<Trade>> weeklyTrades;
+
+    public TradeHistory(final List<Trade> allTrades) {
+        weeklyTrades = new HashMap<>();
+
+        for(int week = 1; week <= 12; week++) {
+            weeklyTrades.put(week, new ArrayList<>());
+        }
+
+        for (Trade indTrade : allTrades) {
+            int month = indTrade.getDate().getMonthValue();
+            List<Trade> listForMonth = weeklyTrades.get(month);
+            listForMonth.add(indTrade);
+        }
+    }
     /**
+     * Returns all trades that fall within the given week index.
+     * Week 0 index = January, Week 1 index = February
      *
-     * @param trade
+     * @param weekNumber = The given month to collect trades for.
+     * @return All of the politician's trades for this week of data.
      */
-    public void addTrade(final Trade trade) {
+    public List<Trade> getWeeklyTrades(final int weekNumber) {
+        if (weekNumber < 1 || weekNumber > 12) {
+            return new ArrayList<>();
+        }
+        return weeklyTrades.get(weekNumber);
     }
 
     /**
-     *
-     * @param week
-     * @param prices
-     * @return
+     * Used to provide all trades from this year for the given politician
+     * @return all trades across all weeks
      */
-    public double getWeeklyProfit(
-            final int week,
-            final Map<String, Double> prices) {
-        return 0.0;
+    public List<Trade> getTrades() {
+        List<Trade> allTrades = new ArrayList<>();
+
+        for(int week = 1; week <=12; week++) {
+            allTrades.addAll(weeklyTrades.get(week));
+        }
+        return allTrades;
     }
 }
--- src/test/java/edu/sandiego/comp305/TradeHistoryTest.java
+++ src/test/java/edu/sandiego/comp305/TradeHistoryTest.java
@@ -2,15 +2,52 @@ package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class TradeHistoryTest {
 
-    @Test
-    void addTrade() {
+    private Trade mockTrade(int month) {
+        Trade trade = mock(Trade.class);
+        LocalDate date = mock(LocalDate.class);
+
+        when(date.getMonthValue()).thenReturn(month);
+        when(trade.getDate()).thenReturn(date);
+
+        return trade;
     }
 
     @Test
-    void getWeeklyProfit() {
+    void tradesPlacedInCorrectWeek() {
+        Trade janTestTrade = mockTrade(1);
+        Trade febTestTrade = mockTrade(2);
+
+        TradeHistory testHistory = new TradeHistory(List.of(janTestTrade,febTestTrade));
+
+        assertEquals(List.of(febTestTrade), testHistory.getWeeklyTrades(2));
+    }
+
+    @Test
+    void emptyListReturnedForInvalidWeek() {
+        Trade janTestTrade = mockTrade(1);
+
+        TradeHistory testHistory = new TradeHistory(List.of(janTestTrade));
+
+        assertTrue(testHistory.getWeeklyTrades(0).isEmpty());
+    }
+
+    @Test
+    void getTradesReturnsMultipleTrades() {
+        Trade testTradeJan = mockTrade(1);
+        Trade testTradeFeb = mockTrade(2);
+        Trade testTradeMar = mockTrade(3);
+
+        TradeHistory testHistory = new TradeHistory(List.of(testTradeJan, testTradeFeb, testTradeMar));
+        List<Trade> collectedTrades = testHistory.getTrades();
+
+        assertTrue(collectedTrades.containsAll(List.of(testTradeJan, testTradeFeb, testTradeMar)));
     }
 }
