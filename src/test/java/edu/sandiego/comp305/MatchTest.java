baaf087d009b64424b911df23c6e3fbb229653e6
devongalb <dgalbraith@sandiego.edu>
2026-05-12 15:10:23 -0700

    fix: corrected tests, since they were not changed when enum was added, test now use enum for results of the matches

--- src/test/java/edu/sandiego/comp305/MatchTest.java
+++ src/test/java/edu/sandiego/comp305/MatchTest.java
@@ -2,10 +2,9 @@ package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,74 +13,80 @@ class MatchTest {
 
     @Test
     void getWinnerReturnsPlayerOne() {
-        final Team playerOneTeam = mock(Team.class);
-        final Team playerTwoTeam = mock(Team.class);
+        final Team playerOneTeam = new Team("Player One");
+        final Team playerTwoTeam = new Team("Player Two");
+        final Politician playerOnePolitician = mock(Politician.class);
+        final Politician playerTwoPolitician = mock(Politician.class);
         final ScoringStrategy strategy = mock(ScoringStrategy.class);
+
+        playerOneTeam.addPolitician(playerOnePolitician);
+        playerTwoTeam.addPolitician(playerTwoPolitician);
+
+        when(strategy.calculateScore(playerOnePolitician, 1)).thenReturn(10.0);
+        when(strategy.calculateScore(playerTwoPolitician, 1)).thenReturn(5.0);
+
         final Match match = new Match(playerOneTeam, playerTwoTeam);
-
-        when(playerOneTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-        when(playerTwoTeam.calculateWeeklyScore(strategy, 1)).thenReturn(5.0);
-
         match.play(strategy, 1);
 
-        assertSame(playerOneTeam, match.getWinner());
+        assertEquals(MatchResults.PLAYER_ONE, match.getWinner());
     }
 
     @Test
     void getWinnerReturnPlayerTwo() {
-        final Team playerOneTeam = mock(Team.class);
-        final Team playerTwoTeam = mock(Team.class);
+        final Team playerOneTeam = new Team("Player One");
+        final Team playerTwoTeam = new Team("Player Two");
+        final Politician playerOnePolitician = mock(Politician.class);
+        final Politician playerTwoPolitician = mock(Politician.class);
         final ScoringStrategy strategy = mock(ScoringStrategy.class);
+
+        playerOneTeam.addPolitician(playerOnePolitician);
+        playerTwoTeam.addPolitician(playerTwoPolitician);
+
+        when(strategy.calculateScore(playerOnePolitician, 1)).thenReturn(5.0);
+        when(strategy.calculateScore(playerTwoPolitician, 1)).thenReturn(10.0);
+
         final Match match = new Match(playerOneTeam, playerTwoTeam);
-
-        when(playerOneTeam.calculateWeeklyScore(strategy, 1)).thenReturn(5.0);
-        when(playerTwoTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-
         match.play(strategy, 1);
 
-        assertSame(playerTwoTeam, match.getWinner());
+        assertEquals(MatchResults.PLAYER_TWO, match.getWinner());
     }
 
     @Test
-    void getWinnerReturnNullWhenTie() {
-        final Team playerOneTeam = mock(Team.class);
-        final Team playerTwoTeam = mock(Team.class);
+    void getWinnerIsTie() {
+        final Team playerOneTeam = new Team("Player One");
+        final Team playerTwoTeam = new Team("Player Two");
+        final Politician playerOnePolitician = mock(Politician.class);
+        final Politician playerTwoPolitician = mock(Politician.class);
         final ScoringStrategy strategy = mock(ScoringStrategy.class);
+
+        playerOneTeam.addPolitician(playerOnePolitician);
+        playerTwoTeam.addPolitician(playerTwoPolitician);
+
+        when(strategy.calculateScore(playerOnePolitician, 1)).thenReturn(5.0);
+        when(strategy.calculateScore(playerTwoPolitician, 1)).thenReturn(5.0);
+
         final Match match = new Match(playerOneTeam, playerTwoTeam);
-
-        when(playerOneTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-        when(playerTwoTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-
         match.play(strategy, 1);
 
-        assertNull(match.getWinner());
-    }
-
-    @Test
-    void isTieReturnsTrue() {
-        final Team playerOneTeam = mock(Team.class);
-        final Team playerTwoTeam = mock(Team.class);
-        final ScoringStrategy strategy = mock(ScoringStrategy.class);
-        final Match match = new Match(playerOneTeam, playerTwoTeam);
-
-        when(playerOneTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-        when(playerTwoTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-
-        match.play(strategy, 1);
-
+        assertEquals(MatchResults.TIE, match.getWinner());
         assertTrue(match.isTie());
     }
 
     @Test
     void isTieFalse() {
-        final Team playerOneTeam = mock(Team.class);
-        final Team playerTwoTeam = mock(Team.class);
+        final Team playerOneTeam = new Team("Player One");
+        final Team playerTwoTeam = new Team("Player Two");
+        final Politician playerOnePolitician = mock(Politician.class);
+        final Politician playerTwoPolitician = mock(Politician.class);
         final ScoringStrategy strategy = mock(ScoringStrategy.class);
+
+        playerOneTeam.addPolitician(playerOnePolitician);
+        playerTwoTeam.addPolitician(playerTwoPolitician);
+
+        when(strategy.calculateScore(playerOnePolitician, 1)).thenReturn(5.0);
+        when(strategy.calculateScore(playerTwoPolitician, 1)).thenReturn(10.0);
+
         final Match match = new Match(playerOneTeam, playerTwoTeam);
-
-        when(playerOneTeam.calculateWeeklyScore(strategy, 1)).thenReturn(10.0);
-        when(playerTwoTeam.calculateWeeklyScore(strategy, 1)).thenReturn(5.0);
-
         match.play(strategy, 1);
 
         assertFalse(match.isTie());
