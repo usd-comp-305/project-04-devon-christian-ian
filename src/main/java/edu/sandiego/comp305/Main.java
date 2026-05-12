7f12ebef13cacce7c21b1ca3ae44594c050a067f
devongalb <dgalbraith@sandiego.edu>
2026-05-12 15:37:56 -0700

    style: initial clean up of main class to turn it into simulator

--- src/main/java/edu/sandiego/comp305/Main.java
+++ src/main/java/edu/sandiego/comp305/Main.java
@@ -18,8 +18,5 @@ public final class Main {
     public static void main(final String[] args) {
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
     }
 }
